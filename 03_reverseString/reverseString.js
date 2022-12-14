0e67508caa47dbe391702eddad729e39151e5f58
Maciejaszek1 <maciek.sygula@gmail.com>
2022-12-14 11:57:57 +0100

    Completed 3rd js exercise. The code is pretty bad so I'll return here later. Hopefully.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let chopped = string.split("");
+    let length = chopped.length - 1;
+    let reversed;
+
+    for (let i = 0 ; length >= 0; length--){
+        reversed += chopped[length];
+    }
+
+    if (string == "") return "";
+
+    else return reversed.slice(9);
 };
 
 // Do not edit below this line

949a2b8be894ac1ceea013141a20bf3ae2a1e63b
Maciejaszek1 <maciek.sygula@gmail.com>
2022-12-14 16:51:59 +0100

    Completed js ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,27 @@
 const sumAll = function(num1, num2) {
 
-    let sum = 0;
+    if (typeof num1 != "number" || 
+        typeof num2 != "number" || 
+        num1<0 || num2<0)
+        
+        return "ERROR";
+    
+    else {
 
+        let sum = 0;
+    
+        if(num1 > num2){
+            let tempNum = num1;
+            num1 = num2;
+            num2 = tempNum;
+        }
 
-    for(;num1 <= num2; num1++){
-        sum += num1;
+        for(;num1 <= num2; num1++){
+            sum += num1;
+        }
+
+        return sum
     }
-
-    return sum;
 };
 
 // Do not edit below this line

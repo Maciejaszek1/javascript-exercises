170cd7ad8efe56ee1e8fd47b9ed4ae926531e313
Maciejaszek1 <maciek.sygula@gmail.com>
2022-12-16 13:27:41 +0100

    Passed 1/6 tests in js ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let half = Math.floor(string.length/2);
 
+    let arr = string.split(string[half]);
+    arr[1] = arr[1].split("").reverse().join("");
+
+    return arr[0] == arr[1];
 };
 
 // Do not edit below this line

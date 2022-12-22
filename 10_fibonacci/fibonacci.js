4712af578b8f6bece6a7fb93be7b8da860f444d8
Maciejaszek1 <maciek.sygula@gmail.com>
2022-12-22 18:49:49 +0100

    Completed js ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(fNum) {
 
+    let num1 = 0;
+    let num2 = 1;
+    let num3 = 1;
+    
+
+    if (fNum < 1) return "OOPS";
+    else
+    for (let i = 2; i<fNum ; i++){
+        
+        num1 = num2;
+        num2 = num3;
+        num3 = num1 + num2;
+
+    }
+
+    return num3;
 };
 
 // Do not edit below this line
